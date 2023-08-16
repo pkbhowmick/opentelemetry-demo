f56729b9f535b925598b4250a957a93febfcce67
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2023-08-16 12:13:22 +0600

    add example
    Signed-off-by: Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	api "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric"
+)
+
+func main() {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	ctx := context.Background()
+
+	// The exporter embeds a default OpenTelemetry Reader and
+	// implements prometheus.Collector, allowing it to be used as
+	// both a Reader and Collector.
+	exporter, err := prometheus.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	provider := metric.NewMeterProvider(metric.WithReader(exporter))
+	meter := provider.Meter("github.com/open-telemetry/opentelemetry-go/example/prometheus")
+
+	// Start the prometheus HTTP server and pass the exporter Collector to it
+	go serveMetrics()
+
+	opt := api.WithAttributes(
+		attribute.Key("A").String("B"),
+		attribute.Key("C").String("D"),
+	)
+
+	// This is the equivalent of prometheus.NewCounterVec
+	counter, err := meter.Float64Counter("foo", api.WithDescription("a simple counter"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	counter.Add(ctx, 5, opt)
+
+	// This is the equivalent of prometheus.NewCounterVec
+	helloCounter, err := meter.Float64Counter("hello", api.WithDescription("hello hit counter"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go serveHelloHandler(helloCounter)
+
+	gauge, err := meter.Float64ObservableGauge("bar", api.WithDescription("a fun little gauge"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = meter.RegisterCallback(func(_ context.Context, o api.Observer) error {
+		n := -10. + rng.Float64()*(90.) // [-10, 100)
+		o.ObserveFloat64(gauge, n, opt)
+		return nil
+	}, gauge)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This is the equivalent of prometheus.NewHistogramVec
+	histogram, err := meter.Float64Histogram("baz", api.WithDescription("a very nice histogram"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	histogram.Record(ctx, 23, opt)
+	histogram.Record(ctx, 7, opt)
+	histogram.Record(ctx, 101, opt)
+	histogram.Record(ctx, 105, opt)
+
+	ctx, _ = signal.NotifyContext(ctx, os.Interrupt)
+	<-ctx.Done()
+}
+
+func serveMetrics() {
+	log.Printf("serving metrics at localhost:8080/metrics")
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("error serving http: %v", err)
+		return
+	}
+}
+
+func serveHelloHandler(counter api.Float64Counter) {
+	http.Handle("/hello", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world!")
+		counter.Add(context.Background(), 1)
+	}))
+}
